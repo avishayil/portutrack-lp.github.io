96fab7e7cf0cf4bf7bd2e7124ced78dc3f4d4889
Avishay Bar <avishay.il@gmail.com>
2021-04-07 01:19:54 +0300

    Pricing update [ci skip]

---
+++ precache-manifest.99f2c84c308f2589f5c68ed9a27f7c59.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da632e205509e5b795f0c31ec45169a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cb5135cbc59fe62d14b",
+    "url": "/static/css/main.8d3012b8.chunk.css"
+  },
+  {
+    "revision": "488845c936173a9f8f1d",
+    "url": "/static/js/2.fb3cfc37.chunk.js"
+  },
+  {
+    "revision": "0def066106b5692cd5c135dcafc5767d",
+    "url": "/static/js/2.fb3cfc37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cb5135cbc59fe62d14b",
+    "url": "/static/js/main.8ba3a31c.chunk.js"
+  },
+  {
+    "revision": "ea630fe81389fbceefbb",
+    "url": "/static/js/runtime-main.bf9f4589.js"
+  },
+  {
+    "revision": "93bfae6516eb9d6244918a617e50be06",
+    "url": "/static/media/multiple-processes-en.93bfae65.jpg"
+  },
+  {
+    "revision": "0cc4fbff1c203ade1fdb12b9d514a864",
+    "url": "/static/media/nacionalidade.0cc4fbff.png"
+  },
+  {
+    "revision": "92694e1160bc61d83d293143f98f88a5",
+    "url": "/static/media/product-main-en.92694e11.png"
+  },
+  {
+    "revision": "e93fba59bbdb341fab6153bd0cff5b56",
+    "url": "/static/media/station-update-en.e93fba59.png"
+  },
+  {
+    "revision": "f6264cb72cb162ebaf0f19071b9ab236",
+    "url": "/static/media/status-update-en.f6264cb7.png"
+  }
+]);
